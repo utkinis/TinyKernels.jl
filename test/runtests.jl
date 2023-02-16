d799add47fdd5ce7d2d3dd8566639caea0deb406
Ludovic Raess <ludovic.rass@gmail.com>
2023-02-16 22:08:00 +0100

    No tests yet

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,22 +4,22 @@ push!(LOAD_PATH, "../src")
 using TinyKernels
 
 function runtests()
-    exename = joinpath(Sys.BINDIR, Base.julia_exename())
-    testdir = pwd()
-    istest(f) = endswith(f, ".jl") && startswith(basename(f), "test_")
-    testfiles = sort(filter(istest, vcat([joinpath.(root, files) for (root, dirs, files) in walkdir(testdir)]...)))
+    # exename = joinpath(Sys.BINDIR, Base.julia_exename())
+    # testdir = pwd()
+    # istest(f) = endswith(f, ".jl") && startswith(basename(f), "test_")
+    # testfiles = sort(filter(istest, vcat([joinpath.(root, files) for (root, dirs, files) in walkdir(testdir)]...)))
 
-    nfail = 0
-    printstyled("Testing TinyKernels.jl\n"; bold=true, color=:white)
+    # nfail = 0
+    # printstyled("Testing TinyKernels.jl\n"; bold=true, color=:white)
 
-    for f in testfiles
-        try
-            run(`$exename -O3 --startup-file=no --check-bounds=no $(joinpath(testdir, f))`)
-        catch ex
-            nfail += 1
-        end
-    end
-    return nfail
+    # for f in testfiles
+    #     try
+    #         run(`$exename -O3 --startup-file=no --check-bounds=no $(joinpath(testdir, f))`)
+    #     catch ex
+    #         nfail += 1
+    #     end
+    # end
+    # return nfail
 end
 
 exit(runtests())
