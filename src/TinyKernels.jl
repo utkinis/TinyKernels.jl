191db79cb42aa8900cf4595b0b8632d65fc2324f
Ludovic Raess <ludovic.rass@gmail.com>
2023-03-29 09:40:51 +0200

    update

--- src/TinyKernels.jl
+++ src/TinyKernels.jl
@@ -34,10 +34,12 @@ include("macros.jl")
 include("CPUBackend.jl")
 
 @static if !isdefined(Base, :get_extension)
-    @require AMDGPU="21141c5a-9bdb-4563-92ae-f87d6854732e" include("../ext/ROCBackend.jl")
-    @require CUDA="052768ef-5323-5732-b1bb-66c8b64840ba" include("../ext/CUDABackend.jl")
-    @require Metal="dde4c033-4e86-420c-a63e-0dd931031962" include("../ext/MetalBackend.jl")
-    @require Enzyme="7da242da-08ed-463a-9acd-ee780be4f1d9" include("../ext/KernelAD.jl")
+    function __init__()
+        @require AMDGPU="21141c5a-9bdb-4563-92ae-f87d6854732e" include("../ext/ROCBackend.jl")
+        @require CUDA="052768ef-5323-5732-b1bb-66c8b64840ba" include("../ext/CUDABackend.jl")
+        @require Enzyme="7da242da-08ed-463a-9acd-ee780be4f1d9" include("../ext/KernelAD.jl")
+        @require Metal="dde4c033-4e86-420c-a63e-0dd931031962" include("../ext/MetalBackend.jl")
+    end
 end
 
 end # module TinyKernels
