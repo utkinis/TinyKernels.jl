ce15e06da1c75df31181f566407f3c154b9113ff
albert-de-montserrat <albert.de.montserrat@gmail.com>
2023-03-30 12:56:27 +0200

    add get_nthreads

--- src/TinyKernels.jl
+++ src/TinyKernels.jl
@@ -16,6 +16,8 @@ Base.similar(::Kernel{BE}, f::F) where {BE,F} = Kernel{BE,F}(f)
 
 @inline ndrange_to_indices(ndrange::Tuple) = CartesianIndices(ndrange)
 @inline ndrange_to_indices(ndrange::AbstractUnitRange) = CartesianIndices((ndrange,))
+@inline get_nthreads(nthreads::Nothing, ndrange) = min(length(ndrange), 256)
+@inline get_nthreads(nthreads, ndrange) = nthreads
 
 const __INDEX__ = gensym("I")
 
